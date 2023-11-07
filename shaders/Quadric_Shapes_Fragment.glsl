76672346c635171df1db16eaf2e2aeecfcad0548
Erich Loftis <erichlof@swbell.net>
2023-11-07 01:11:27 -0600

    Update Quadric_Shapes_Fragment.glsl

--- shaders/Quadric_Shapes_Fragment.glsl
+++ shaders/Quadric_Shapes_Fragment.glsl
@@ -30,8 +30,9 @@ vec3 rayOrigin, rayDirection;
 vec3 intersectionNormal;
 vec2 intersectionUV;
 int intersectionTextureID;
+int intersectionShapeIsClosed;
 
-struct Material { int type; vec3 color; vec3 color2; float roughness; float IoR; int textureID; };
+struct Material { int type; int isCheckered; vec3 color; vec3 color2; float roughness; float IoR; int textureID; };
 struct UnitRectangle { vec2 uvScale; Material material; };
 struct Box { vec3 minCorner; vec3 maxCorner; vec2 uvScale; Material material; };
 struct UnitSphere { vec2 uvScale; Material material; };
@@ -118,6 +119,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uRectangleInvMatrix)) * normal;
 		intersectionMaterial = rectangles[0].material;
 		intersectionUV = vec2(u, v) * rectangles[0].uvScale;
+		intersectionShapeIsClosed = FALSE;
 	}
 	
 	
@@ -132,6 +134,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uDiffuseSphereInvMatrix)) * normal;
 		intersectionMaterial = spheres[0].material;
 		intersectionUV = calcUnitSphereUV(intersectionPoint) * spheres[0].uvScale;
+		intersectionShapeIsClosed = TRUE;
 	}
 
 	// transform ray into Metal Unit Sphere's object space
@@ -145,6 +148,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uMetalSphereInvMatrix)) * normal;
 		intersectionMaterial = spheres[1].material;
 		intersectionUV = calcUnitSphereUV(intersectionPoint) * spheres[1].uvScale;
+		intersectionShapeIsClosed = TRUE;
 	}
 
 	// transform ray into ClearCoat Unit Sphere's object space
@@ -158,6 +162,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uCoatSphereInvMatrix)) * normal;
 		intersectionMaterial = spheres[2].material;
 		intersectionUV = calcUnitSphereUV(intersectionPoint) * spheres[2].uvScale;
+		intersectionShapeIsClosed = TRUE;
 	}
 
 	// transform ray into Glass Unit Sphere's object space
@@ -171,6 +176,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uGlassSphereInvMatrix)) * normal;
 		intersectionMaterial = spheres[3].material;
 		intersectionUV = calcUnitSphereUV(intersectionPoint) * spheres[3].uvScale;
+		intersectionShapeIsClosed = TRUE;
 	}
 
 	// transform ray into Unit Cylinder's object space
@@ -184,6 +190,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uCylinderInvMatrix)) * normal;
 		intersectionMaterial = cylinders[0].material;
 		intersectionUV = calcUnitCylinderUV(intersectionPoint) * cylinders[0].uvScale;
+		intersectionShapeIsClosed = FALSE;
 	}
 
 	// transform ray into Unit Cone's object space
@@ -197,6 +204,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uConeInvMatrix)) * normal;
 		intersectionMaterial = cones[0].material;
 		intersectionUV = calcUnitCylinderUV(intersectionPoint) * cones[0].uvScale;
+		intersectionShapeIsClosed = FALSE;
 	}
 
 	// transform ray into Unit Paraboloid's object space
@@ -210,6 +218,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uParaboloidInvMatrix)) * normal;
 		intersectionMaterial = paraboloids[0].material;
 		intersectionUV = calcUnitCylinderUV(intersectionPoint) * paraboloids[0].uvScale;
+		intersectionShapeIsClosed = FALSE;
 	}
 
 	// transform ray into Unit Hyperboloid's object space
@@ -223,6 +232,7 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionNormal = transpose(mat3(uHyperboloidInvMatrix)) * normal;
 		intersectionMaterial = hyperboloids[0].material;
 		intersectionUV = calcUnitCylinderUV(intersectionPoint) * hyperboloids[0].uvScale;
+		intersectionShapeIsClosed = FALSE;
 	}
 
 	// transform ray into Unit Hyperbolic Paraboloid's object space
@@ -235,7 +245,8 @@ float SceneIntersect( int isShadowRay, int sceneUsesDirectionalLight )
 		intersectionPoint = rObjOrigin + t * rObjDirection;
 		intersectionNormal = transpose(mat3(uHyperbolicParaboloidInvMatrix)) * normal;
 		intersectionMaterial = hyperbolicParaboloids[0].material;
-		intersectionUV = calcUnitCylinderUV(intersectionPoint) * hyperbolicParaboloids[0].uvScale;
+		intersectionUV = vec2(intersectionPoint.x, intersectionPoint.z) * hyperbolicParaboloids[0].uvScale;
+		intersectionShapeIsClosed = FALSE;
 	}
 
 
@@ -257,7 +268,7 @@ vec3 RayTrace()
 //-------------------------------------------------------------------------------------------
 {
 	float pointLightPower = 4.0;
-        Material pointLightMaterial = Material( POINT_LIGHT, vec3(1.0) * pointLightPower, vec3(1.0), 0.0, 0.0, -1 );
+        Material pointLightMaterial = Material( POINT_LIGHT, FALSE, vec3(1.0) * pointLightPower, vec3(1.0), 0.0, 0.0, -1 );
 	vec3 pointLightPosition = vec3(4, 10, 5);
 	vec3 directionToSunlight = normalize(vec3(-1, 1, 0.5));
 	float sunlightPower = 6.0;
@@ -290,16 +301,16 @@ vec3 RayTrace()
 
 	int isShadowRay = FALSE;
 	int willNeedReflectionRay = FALSE;
-	int sceneUsesDirectionalLight = TRUE;//FALSE;
-	//int previousIntersectionMaterialType;
-	//intersectionMaterial.type = -100;
+	int sceneUsesDirectionalLight = TRUE;
+	int previousIntersectionMaterialType;
+	intersectionMaterial.type = -100;
 
 	// For the kind of ray tracing we're doing, 6 or 7 bounces is enough to do all the reflections and refractions that are most noticeable.
 	// You might be able to get away with 5 bounces if on a mobile budget, or crank it up to 8/9 bounces if your scene has a bunch of mirrors or glass objects.
 
 	for (int bounces = 0; bounces < 8; bounces++)
 	{
-		//previousIntersectionMaterialType = intersectionMaterial.type;
+		previousIntersectionMaterialType = intersectionMaterial.type;
 
 		// the following tests for intersections with the entire scene, then reports back the closest object (min t value)
 		t = SceneIntersect( isShadowRay, sceneUsesDirectionalLight );
@@ -413,6 +424,11 @@ vec3 RayTrace()
 			textureColor *= textureColor; // remove image gamma by raising texture color to the power of 2.2 (but squaring is close enough and cheaper)
 			intersectionMaterial.color *= textureColor; // now that the texture color is in linear space, we can do simple math with it, like multiplying and adding
 		}
+
+		if (intersectionMaterial.isCheckered == TRUE)
+		{
+			intersectionMaterial.color = mod(floor(intersectionUV.x) + floor(intersectionUV.y), 2.0) == 0.0 ? intersectionMaterial.color : intersectionMaterial.color2;
+		}
 		
                 if (intersectionMaterial.type == PHONG)
                 {
@@ -456,16 +472,7 @@ vec3 RayTrace()
 			continue; // continue ray tracing next with the mirror-reflection ray
 		}
 
-		if (intersectionMaterial.type == CHECKERED_CLEARCOAT)
-		{
-			if (mod(floor(intersectionUV.x) + floor(intersectionUV.y), 2.0) == 1.0)
-				intersectionMaterial.color = intersectionMaterial.color2;
-			// now that the checkered color has been determined for this surface, change the material type
-			// to the usual ClearCoat, in order to handle lighting and reflections (avoids code duplication)
-			intersectionMaterial.type = CLEARCOAT_DIFFUSE; // will be handled by the code directly below
-		}
-
-		if (intersectionMaterial.type == CLEARCOAT_DIFFUSE)
+		if (intersectionMaterial.type == CLEARCOAT)
 		{
 			// the following will decrease the IndexOfRefracton, or IoR(shininess) of the clear coating as the roughness increases 
 			intersectionMaterial.IoR = mix(1.0, intersectionMaterial.IoR, clamp(1.0 - (intersectionMaterial.roughness * 1.2), 0.001, 1.0)); // as roughness increases, the IndexOfRefraction(shininess/brightness) decreases
@@ -481,10 +488,10 @@ vec3 RayTrace()
 			
 			specularContribution = doBlinnPhongSpecularLighting(rayColorMask, rayDirection, shadingNormal, directionToLight, lightColor, intersectionMaterial);
 
-			// If this ClearCoat type of material is the first thing that the camera ray encounters (bounces == 0), then setup and save a reflection ray for later use.
-			// After we've done that, first we'll send out the usual shadow ray to see if the Diffuse and Specular contributions can be added. Then once the shadow ray 
+			// If this ClearCoat type of material is either the first thing that the camera ray encounters (bounces == 0), or the 2nd thing the ray encounters after reflecting from METAL (bounces == 1),
+			// then setup and save a reflection ray for later use. After we've done that, first we'll send out the usual shadow ray to see if the Diffuse and Specular contributions can be added. Then once the shadow ray 
 			// is done testing for light visibility, we'll rewind back to this surface and send out the saved reflection ray on its own trajectory, in order to capture reflections of the environment. 
-			if (bounces == 0)
+			if (bounces == 0 || (bounces == 1 && previousIntersectionMaterialType == METAL))
 			{
 				willNeedReflectionRay = TRUE; // this flag will let the future code know that it needs to rewind time and trace the saved reflection ray
 				reflectionRayColorMask = rayColorMask * reflectance * clamp(1.0 - (intersectionMaterial.roughness * 1.2), 0.001, 1.0); // weight reflected ray with reflectance value we got
@@ -513,11 +520,11 @@ vec3 RayTrace()
 			specularContribution = (isShadowRay == TRUE) ? vec3(0) : specularContribution;
 			accumulatedColor += specularContribution; // ideally we would send a new shadow ray to see if this surface can actually see the light, but to minimize complexity/time, we just add specular highlight now
 
-			// If this Transparent type of material is the first thing that the camera ray encounters (bounces == 0), then setup and save a reflection ray for later use.
-			// After we've done that, first we'll send out the refracted ray (transmitted ray) to find what parts of the scene we can see through the material. Then once the refraction
-			// (transmitted) ray is done with its work, we'll send out the saved reflection ray on its own trajectory, in order to capture reflections of the surrounding environment. 
+			// If this Transparent type of material is either the first thing the camera ray encounters (bounces == 0) or the 2nd thing the ray encounters after reflecting from METAL (bounces == 1) then setup 
+			// and save a reflection ray for later use. After we've done that, first we'll send out the refracted ray (transmitted ray) to find what parts of the scene we can see through the material. Then once the 
+			// refraction(transmitted) ray is done with its work, we'll send out the saved reflection ray on its own trajectory, in order to capture reflections of the surrounding environment. 
 			// In the end, this will give us the photo-realistic double image (reflected portion & refracted portion) that we see on real-life transparent materials like glass, windows, water, plastic, etc.
-			if (bounces == 0)
+			if (bounces == 0 || (bounces == 1 && previousIntersectionMaterialType == METAL))
 			{
 				willNeedReflectionRay = TRUE;
 				reflectionRayColorMask = rayColorMask * reflectance * clamp(1.0 - (intersectionMaterial.roughness * 1.2), 0.001, 1.0);
@@ -535,10 +542,10 @@ vec3 RayTrace()
 			}
 
 			// This part of the code handles the transmitted(refracted) portion of the Transparent surface
-
-			// is ray leaving a solid object from the inside? 
-			// If so, attenuate ray color with object color by how far ray has travelled through the object's interior
-			if (distance(geometryNormal, shadingNormal) > 0.1) // this happens when trying to exit a glass sphere from the inside - geometryNormal and shadingNormal do not match
+			if (intersectionShapeIsClosed == FALSE)
+				rayColorMask *= intersectionMaterial.color;
+			// Is ray leaving a solid object from the inside? If so, attenuate ray color with object color by how far ray has travelled through the object's interior
+			else if (distance(geometryNormal, shadingNormal) > 0.1) // this happens when trying to exit a glass sphere from the inside - geometryNormal and shadingNormal do not match
 			{
 				transparentThickness = 0.2;
 				// the following uses Beer's Law to attenuate the light energy (brightness and color saturation) by how far the light ray had to travel through the transparent material.
@@ -546,6 +553,7 @@ vec3 RayTrace()
 				// As the material gets thicker, like thick glass or a deep pool of colored water, the transmitted light will lose more energy(darker) and will take on much more of the transparent material's color.
 				rayColorMask *= exp( log(clamp(intersectionMaterial.color, 0.01, 0.99)) * transparentThickness * t ); // Use Beer's Law to attenuate light based on how far the ray traveled through the medium
 			}
+			
 			// the refraction through the surface only accounts for the portion of light that is transmitted, so we must weight it accordingly (Reflectance vs. Transmittance)
 			rayColorMask *= transmittance;
 			rayColorMask *= clamp(1.0 - (intersectionMaterial.roughness * 1.2), 0.001, 1.0); // inverse relationship: as roughness increases, refraction(transmission) brightness decreases
@@ -594,29 +602,29 @@ void SetupScene(void)
 	// rgb values for common metals
 	// Gold: (1.000, 0.766, 0.336) / Aluminum: (0.913, 0.921, 0.925) / Copper: (0.955, 0.637, 0.538) / Silver: (0.972, 0.960, 0.915)
 	
-	Material uvGridMaterial = Material(CLEARCOAT_DIFFUSE, vec3(1.0), vec3(1.0), 0.0, 1.3, 0 );
-	Material redMaterial = Material(PHONG, vec3(0.7, 0.01, 0.01), vec3(1.0), 1.0, 0.0, -1 );
-	Material metalMaterial = Material(METAL, vec3(1.000, 0.766, 0.336), vec3(1.0), 0.0, 0.0, -1 );
-	Material glassMaterial = Material(TRANSPARENT, vec3(0.4, 1.0, 0.6), vec3(1.0), 0.0, 1.5, -1 );
-	Material grayBlackCheckerMaterial = Material(CHECKERED_CLEARCOAT, vec3(0.5), vec3(0.001), 0.0, 1.4, -1 );
-	Material whiteRedCheckerMaterial = Material(CHECKERED_CLEARCOAT, vec3(0.8), vec3(0.5, 0.01, 0.01), 0.0, 1.4, -1 );
+	Material uvGridMaterial = Material(CLEARCOAT, FALSE, vec3(1.0, 1.0, 1.0), vec3(0.0, 0.0, 0.0), 0.0, 1.3, 0 );
+	Material redMaterial = Material(PHONG, FALSE, vec3(0.7, 0.01, 0.01), vec3(0.0, 0.0, 0.0), 1.0, 0.0, -1 );
+	Material metalMaterial = Material(METAL, FALSE, vec3(0.955, 0.637, 0.538), vec3(0.913, 0.921, 0.925), 0.0, 0.0, -1 );
+	Material glassMaterial = Material(TRANSPARENT, FALSE, vec3(0.4, 1.0, 0.6), vec3(0.0, 0.0, 0.0), 0.0, 1.5, -1 );
+	Material grayBlackCheckerMaterial = Material(CLEARCOAT, TRUE, vec3(0.5), vec3(0.001, 0.001, 0.001), 0.0, 1.4, -1 );
+	Material whiteRedCheckerMaterial = Material(CLEARCOAT, TRUE, vec3(0.8), vec3(0.5, 0.01, 0.01), 0.0, 1.4, -1 );
 	// vec3 pointLightPosition = vec3(4, 10, 5);
 	// boxes[0] = Box(pointLightPosition - vec3(0.5), pointLightPosition + vec3(0.5), vec2(1, 1), pointLightMaterial);
 
 	rectangles[0] = UnitRectangle(vec2(300, 300), grayBlackCheckerMaterial);
 
-	spheres[0] = UnitSphere(vec2(1, 1), redMaterial);
-	spheres[1] = UnitSphere(vec2(1, 1), metalMaterial);
+	spheres[0] = UnitSphere(vec2(2, 1), redMaterial);
+	spheres[1] = UnitSphere(vec2(2, 1), metalMaterial);
 	spheres[2] = UnitSphere(vec2(2, 1), uvGridMaterial);
-	spheres[3] = UnitSphere(vec2(1, 1), glassMaterial);
+	spheres[3] = UnitSphere(vec2(2, 1), glassMaterial);
 
 	cylinders[0] = UnitCylinder(vec2(2, 1), uvGridMaterial);
-	cylinders[0].material.IoR = 1.3; // decrease the shininess just a bit
+	///cylinders[0].material.IoR = 1.3; // decrease the shininess just a bit
 
-	cones[0] = UnitCone(vec2(6, 4), whiteRedCheckerMaterial);
-	paraboloids[0] = UnitParaboloid(vec2(6, 4), whiteRedCheckerMaterial);
-	hyperboloids[0] = UnitHyperboloid(vec2(6, 4), whiteRedCheckerMaterial);
-	hyperbolicParaboloids[0] = UnitHyperbolicParaboloid(vec2(6, 4), whiteRedCheckerMaterial);
+	cones[0] = UnitCone(vec2(8, 5), whiteRedCheckerMaterial);
+	paraboloids[0] = UnitParaboloid(vec2(8, 5), whiteRedCheckerMaterial);
+	hyperboloids[0] = UnitHyperboloid(vec2(8, 6), whiteRedCheckerMaterial);
+	hyperbolicParaboloids[0] = UnitHyperbolicParaboloid(vec2(4, 4), whiteRedCheckerMaterial);
 }
 
 
