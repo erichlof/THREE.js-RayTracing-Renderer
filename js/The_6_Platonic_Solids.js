0436f000d4f939c26bb0972536ef7ba50c562524
Erich Loftis <erichlof@swbell.net>
2025-04-25 17:55:49 -0500

    Update The_6_Platonic_Solids.js

--- js/The_6_Platonic_Solids.js
+++ js/The_6_Platonic_Solids.js
@@ -90,7 +90,7 @@ function initSceneData()
 	sceneIsDynamic = false;
 	
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.75;
+	pixelRatio = mouseControl ? 1.0 : 0.75;
 	
 	EPS_intersect = 0.01;
 	
